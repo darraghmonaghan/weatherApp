7f7d30ffb882e7492b98bcf68de5115ce03f8b44
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-01-07 12:01:13 +0000

    basic rendering of date/time on search results page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,24 @@
 class SearchesController < ApplicationController
   
+# require 'httparty'
+
   def index
   	render('index')
   end
 
   def forecast
-  	@data = params[:data][:city]
-  	puts @data
+  	require 'httparty'
+
+  	target_city = params[:data][:city]
+  	puts target_city
+  	target_url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + target_city + '&units=metric&cnt=16&APPID=bd2ae3f21976f0f86cc5e88e358b72bd'
+  	response = HTTParty.get(target_url)
+  	response.parsed_response
+  	puts response.parsed_response
+  	@results = response.parsed_response
+
   	render('forecast')
+
   end
 
   def detail
