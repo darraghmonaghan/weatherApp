f187eb89835eed206821ee232499a78d81ac5cec
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-01-08 16:48:06 +0000

    fixed search bug - removing white space in city name before API request

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,15 +11,15 @@ class SearchesController < ApplicationController
     @apiKey = 'bd2ae3f21976f0f86cc5e88e358b72bd'
 
   	@target_city = params[:data][:city]
+    
+    @city_search = params[:data][:city].delete(' ')
     @unit = params[:data][:unit]
 
-  	target_url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + @target_city + '&units=' + @unit + '&cnt=16&APPID=' + @apiKey
+  	target_url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + @city_search + '&units=' + @unit + '&cnt=16&APPID=' + @apiKey
   	response = HTTParty.get(target_url)
   	response.parsed_response
-  	puts response.parsed_response
   	@results = response.parsed_response
   	render('forecast')
-
   end
 
   private
