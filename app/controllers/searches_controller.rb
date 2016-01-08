46b3456d7ca25d6428e6a5d4701f74d88fcf4448
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-01-08 15:23:10 +0000

    added celcius or farenheit option

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,8 +8,12 @@ class SearchesController < ApplicationController
   def forecast
   	require 'httparty'
 
+    @apiKey = 'bd2ae3f21976f0f86cc5e88e358b72bd'
+
   	@target_city = params[:data][:city]
-  	target_url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + @target_city + '&units=metric&cnt=16&APPID=bd2ae3f21976f0f86cc5e88e358b72bd'
+    @unit = params[:data][:unit]
+
+  	target_url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + @target_city + '&units=' + @unit + '&cnt=16&APPID=' + @apiKey
   	response = HTTParty.get(target_url)
   	response.parsed_response
   	puts response.parsed_response
@@ -17,10 +21,6 @@ class SearchesController < ApplicationController
   	render('forecast')
 
   end
-
-  # def detail
-  # 	@detail_data = params[:min]
-  # end
 
   private
 
